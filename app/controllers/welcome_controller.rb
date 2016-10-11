b8cf70a1878e922554289a40d36b518f914b6765
nishina <n.toshi.1.014@gmail.com>
2016-10-11 23:46:41 +0900

    add search function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,18 @@ class WelcomeController < ApplicationController
   PER = 3
 
   def index
-    @events = Event.page(params[:page]).per(PER).all
+    @q = Event.page(params[:page]).per(PER).order(:start_time).search(search_params)
+    @events = @q.result(distinct: true)
+    # @events = Event.page(params[:page]).per(PER).all
     # @events = Event.page(params[:page]).per(PER).where('start_time > ?', Time.zone.now).order(:start_time)
   end
+
+  private
+
+  def search_params
+    params.require(:q).permit(:name_cont, :start_time_gteq)
+  rescue
+    { start_time_gteq: Time.zone.now}
+  end
 end
+
