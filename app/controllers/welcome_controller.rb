eeddc002bd227fd049e8fadfc5a6d4988505dd58
nishina <n.toshi.1.014@gmail.com>
2016-10-11 23:05:09 +0900

    add kaminari

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
+  PER = 3
+
   def index
-    @events = Event.all
-    # @events = Event.where('start_time > ?', Time.zone.now).order(:start_time)
+    @events = Event.page(params[:page]).per(PER).all
+    # @events = Event.page(params[:page]).per(PER).where('start_time > ?', Time.zone.now).order(:start_time)
   end
 end
